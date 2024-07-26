b87d6a7e76ded1755b6a2de9e2f0df85b935300d
Tiago Pontes <76593718+T-Pontes@users.noreply.github.com>
2024-07-26 13:13:00 -0300

    Adicionar arquivos de projeto.

---
+++ Fina.Core/Responses/Response.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Fina.Core.Responses
+{
+    public class Response<TData>
+    {
+        [JsonConstructor] //Para tornar este construtor como padrão para serialização Json no .NET
+        public Response() => _code = Configuration.DefaultStatusCode;
+
+        public Response(
+            TData? data, 
+            int code = Configuration.DefaultStatusCode, 
+            string? message = null)
+        {
+            Data = data;
+            _code = code;
+            Message = message;
+        }
+        
+        private int _code = Configuration.DefaultStatusCode;
+
+        public TData? Data { get; set; }
+        public string? Message { get; set; }
+
+        [JsonIgnore] //Para receber a informação sem exibir na tela 
+        public bool IsSuccess => _code is >= 200 and <= 299;
+    }
+}
